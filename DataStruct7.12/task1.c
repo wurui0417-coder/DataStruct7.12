64b457e9500e48de3d49699055a1bc1ee37f5573
锐 吴 <wurui0417@gmail.com>
2026-07-13 15:28:17 +0800

    添加项目文件。

---
+++ DataStruct7.12/task1.c
@@ -0,0 +1,34 @@
+//#include<stdio.h>
+//
+//#define MAX 100
+//typedef int Elemtype;
+//
+//typedef struct {
+//	Elemtype* data;
+//	int length;
+//}SeqList;
+//
+//SeqList* Initlist()
+//{
+//	SeqList* L = (SeqList*)malloc(sizeof(SeqList));
+//	L->data = (Elemtype*)malloc(sizeof(Elemtype) * MAX);
+//	L->length = 0;
+//	return L;
+//}
+//
+//void AddElem()
+//{
+//
+//}
+//
+//
+//int main()
+//{
+//	SeqList* List = Initlist();
+//	
+//	AddElem(List, 1);
+//}
+
+
+
+
