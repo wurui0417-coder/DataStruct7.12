a04a772680baef5bba9ce4bbd37b5af624025e52
锐 吴 <wurui0417@gmail.com>
2026-07-14 18:04:30 +0800

    完善单链表遍历打印代码

--- DataStruct7.12/task1.c
+++ DataStruct7.12/task1.c
@@ -1,7 +1,6 @@
-//#include<stdio.h>
 //
 //#define MAX 100
-//typedef int Elemtype;
+
 //
 //typedef struct {
 //	Elemtype* data;
@@ -28,7 +27,71 @@
 //	
 //	AddElem(List, 1);
 //}
+#include<stdlib.h>
+#include<stdio.h>
 
+typedef int ElemType;
 
+typedef struct node {
+	ElemType data;
+	struct node* next;
+}Node,*LinkList;
 
+//初始化链表
+LinkList iniList()
+{
+	LinkList head = (LinkList)malloc(sizeof(Node));
+	head->data = 0;
+	head->next = NULL;
+	return head;
+}
 
+//Head insertion
+void headinsert(LinkList L, ElemType e)
+{
+	LinkList l1 = (LinkList)malloc(sizeof(Node));
+	l1->data = e;
+	l1->next = L->next;
+	L->next = l1;
+}
+
+//list node
+void ListNode(LinkList L)
+{
+	LinkList p = L->next;
+	while (p!= 0)
+	{
+		printf("%d\n", *p);
+		p = p->next;
+	}
+}
+
+//get tail
+LinkList get_tail(LinkList L)
+{
+	LinkList p = L;
+	while (p->next != NULL)
+	{
+		p = p->next;
+	}
+	return p;
+}
+
+//end insertion
+void endinsert(LinkList L, ElemType e)
+{
+	LinkList l1 = (LinkList)malloc(sizeof(Node));
+	l1->data = e;
+	l1->next = L->next;
+	L->next = l1;
+}
+
+int main()
+{
+	LinkList list = iniList();
+	headinsert(list, 10);
+	headinsert(list, 20);
+	headinsert(list, 30);
+	endinsert(get_tail(list), 5);
+	ListNode(list);
+}
